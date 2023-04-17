c4f254c42056bd27883bb90ede19d1a3e59c7395
Cameron Reikes <cameronreikes@gmail.com>
2023-04-17 11:47:29 -0700

    Release build flag

--- build_desktop.bat
+++ build_desktop.bat
@@ -2,6 +2,15 @@
 
 echo If this fails make sure you cloned with submodules (or go download the submodules) then run build_libclang.bat and build_raylib.bat
 
+
+set ARGS=""
+
+if "%1" == "release" (
+ set ARGS=/Fe"introspect_release" /O2
+) else (
+ set ARGS=/Fe"introspect_debug" /Zi
+)
+
 cl^
  thirdparty\llvm-project\build-release-static\lib\*.lib^
  /Ithirdparty\llvm-project\clang\include^
@@ -9,5 +18,11 @@ cl^
  /Ithirdparty\raylib\build-release\raylib\include^
  /Ithirdparty^
  /MT gdi32.lib user32.lib shell32.lib msvcrt.lib vcruntime.lib Winmm.lib Version.lib^
- /Fe"introspect_debug"^
- /W3 /Zi /WX main.c 
+ /W3 /WX %ARGS% main.c 
+
+
+if "%1" == "release" (
+ echo Built release build
+) else (
+ echo Built debug build. For release do `build_desktop.bat release`
+)
